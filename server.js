6dc1068ff59d98d80c0fa45412c13d6fcda4d2b3
joxar <mfpirlo@yahoo.co.jp>
2017-12-12 00:03:13 +0900

    b

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
-var http = require('http');
-var s = require('./settings');
-var server = http.createServer();
-server.on('request', function(req, res) {
+var http = require('http'),
+	qs = require('querystring'),
+	fs = require('fs'),
+	ejs = require('ejs'),
+	s = require('./settings'),
+	template = fs.readFileSync(__dirname + 'html/index.ejs', 'utf-8'),
+	server = http.createServer(),
+	posts = [];
+
+/*** diplay form ***/
+function renderForm(posts, res) {
+	var data = ejs.render(template, {
+		posts: posts
+	});
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write("hello world2!");
+	res.write(data);
 	res.end();
+}
+
+server.on('request', function(req, res) {
+	if (req.method === 'POST') {
+		req.data = '';
+		// receive posts data
+		req.on('readable', function() {
+			req.data = req.read() || '';
+			console.log(req.data);
+		});
+		// end
+		req.on('end', function() {
+			var query = qs.parse(req.data);
+			console.log(query);
+			posts.push(query.user_name);
+			renderForm(posts, res);
+		});
+	} else {
+		renderForm(posts, res);
+	}
 });
 // server.listen(s.port, s.host);
 server.listen(process.env.PORT || 8080);
